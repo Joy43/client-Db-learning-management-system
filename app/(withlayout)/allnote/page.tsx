3a6d927e694dcb9e59b5d288f9ce9c52faa1c541
joy43 <ssjoy43@gmail.com>
2024-05-11 00:40:37 +0600

    download option add pdf file for google drive

---
+++ app/(withlayout)/allnote/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+import React from 'react';
+
+function Allnote() {
+    const files = [
+        {
+          id: '1pFY8OdzoVVsEknCQp0MXx6PQHd2w-xZ2',
+          name: 'c ++ datatype',
+          downloads: '2k'
+        },
+        {
+          id: '1-zO06KiF-c7Ca39reFkmmr19aPhsZkIN',
+          name: 'Code Node js',
+          downloads: '15k'
+        },
+        {
+          id: '1MyVqzqr6pyV4hCjOArNMu5sKTLmG_HRB',
+          name: 'React native guide',
+          downloads: '10k'
+        },
+        {
+          id: '1jGdFO1mjrTfHySscHWM-H-32Vb4KaUSD',
+          name: 'system design',
+          downloads: '5k'
+        }
+      ];
+
+  const handleDownload = (fileID:string) => {
+    const downloadLink = `https://drive.google.com/uc?export=download&id=${fileID}`;
+    const downloadElement = document.createElement('a');
+    downloadElement.href = downloadLink;
+    downloadElement.click();
+  };
+
+  return (
+    <div className='grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 min-h-screen bg-purple-900'>
+      {files.map((file, index) => (
+        <div key={index} onClick={() => handleDownload(file.id)} className="flex flex-row m-auto bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 p-6 gap-8 rounded-lg border-2 border-purple-500 cursor-pointer">
+          <div className="my-auto">
+          <div className="text-lg text-purple-300">{file.name}</div>
+            <div className="text-4xl text-purple-100">{file.downloads}</div>
+          </div>
+          <div className="text-purple-300 my-auto bg-gradient-to-l from-purple-700 via-purple-800 to-purple-900 rounded-full p-4">
+            {/* Download Icon */}
+            <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+            </svg>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Allnote;
+
--- app/components/Classnote.tsx
+++ app/components/Classnote.tsx
@@ -1,25 +1,29 @@
-
 import { Button } from 'flowbite-react';
 import Image from 'next/image';
 import React from 'react';
-import Notedemo from '@/public/images/note.png'
+import Notedemo from '@/public/images/note.png';
+import Link from 'next/link';
 
-const Classnote:React.FC = () => {
+const Classnote: React.FC = () => {
     return (
-        <div className='  bg-[#17193b] text-white justify-around 
-        flex rounded-full' data-aos="flip-left">
+        <div className='bg-[#17193b] text-white justify-around flex rounded-full' data-aos="flip-left">
             {/*---------- content------------ */}
-<div className=' m-2 ml-8'>
-
-<div className='mt-4'><h1>Best teacher made class notes and
-    <br /> Need lecture sheets?</h1></div>
-<div className='mt-6'><Button>Free Download </Button></div>
-</div>
-{/*------ image---------- */}
-<div className='p-4'>
-    <Image src={Notedemo}  width={300}
-      height={300} alt='Note '/>
-</div>
+            <div className='m-2 ml-8'>
+                <div className='mt-4'>
+                    <h1>Best teacher made class notes and<br /> Need lecture sheets?</h1>
+                </div>
+                <div className='mt-6'>
+                    <Link href="/allnote" passHref>
+                        <Button className='flex'>
+                            <span>Free Download</span>
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+            {/*------ image---------- */}
+            <div className='p-4'>
+                <Image src={Notedemo} width={300} height={300} alt='Note ' />
+            </div>
         </div>
     );
 };
